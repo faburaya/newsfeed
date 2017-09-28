9bb269d918e6172ddc25e8c8c70437f479dec3b1
Felipe Aburaya <felipe.aburaya@hotmail.com>
2017-09-28 13:48:09 +0000

    fixing scripts

--- aws_linux_ami_setup_prod_env.sh
+++ aws_linux_ami_setup_prod_env.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 wget https://s3-sa-east-1.amazonaws.com/faburaya-builds/aws_newsfeed_v1_2017.09/newsfeed
+chmod +x newsfeed
+mv newsfeed /etc/init.d/
+chkconfig --add newsfeed
 wget https://s3-sa-east-1.amazonaws.com/faburaya-builds/aws_newsfeed_v1_2017.09/newsfeed_server
 wget https://s3-sa-east-1.amazonaws.com/faburaya-builds/aws_newsfeed_v1_2017.09/newsfeed_server.config
-chmod +x newsfeed newsfeed_server
-mv newsfeed /etc/init.d/
+chmod +x newsfeed_server
 mkdir /opt/newsfeed
 mv newsfeed* /opt/newsfeed/
-chkconfig --add newsfeed
 /etc/init.d/newsfeed start
