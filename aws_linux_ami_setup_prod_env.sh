fe78253e1962804470098a2f1701d858ac7c4e33
Felipe Vieira Aburaya <felipe.aburaya@hotmail.com>
2017-09-27 23:40:27 -0300

    added more scripts for setup of prod env in AWS EC2

--- aws_linux_ami_setup_prod_env.sh
+++ aws_linux_ami_setup_prod_env.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+
+wget https://s3-sa-east-1.amazonaws.com/faburaya-builds/aws_newsfeed_v1_2017.09/newsfeed
 wget https://s3-sa-east-1.amazonaws.com/faburaya-builds/aws_newsfeed_v1_2017.09/newsfeed_server
 wget https://s3-sa-east-1.amazonaws.com/faburaya-builds/aws_newsfeed_v1_2017.09/newsfeed_server.config
-chmod +x newsfeed_server
-sudo ./newsfeed_server
+chmod +x newsfeed newsfeed_server
+mv newsfeed /etc/init.d/
+chkconfig --add newsfeed
+/etc/init.d/newsfeed start
